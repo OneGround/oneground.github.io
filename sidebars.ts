c0d6d7f641646ef5109ce8495535de5b9fd0b24a
Johannes Battjes <johannes.battjes@roxit.nl>
2026-03-26 08:56:06 +0100

    bsn

--- sidebars.ts
+++ sidebars.ts
@@ -98,6 +98,24 @@ const sidebars: SidebarsConfig = {
                     label: "Document Upload Guide"
                 }
             ]
+        },
+        {
+            type: "category",
+            label: "Cases",
+            items: [
+                {
+                    type: "doc",
+                    id: "cases/use-of-bsn",
+                    label: "Use of BSN"
+                }
+            ]
+        }
+    ],
+    changelog: [
+        {
+            type: "doc",
+            id: "changelog",
+            label: "Changelog"
         }
     ]
 };
